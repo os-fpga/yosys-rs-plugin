40bd7942cf408a953cbfc8c6b6c3383fed3bd6d4
Aram Kostanyan <aram@rapidsilicon.com>
2022-02-27 19:04:00 +0500

    Added DFF inference similiar to QL k6_n10.

---
+++ common/cells_sim.v
@@ -0,0 +1,38 @@
+// Copyright (C) 2022 RapidSilicon
+//
+
+module inv (
+    output Q,
+    input  A
+);
+  assign Q = A ? 0 : 1;
+endmodule
+
+module buff (
+    output Q,
+    input  A
+);
+  assign Q = A;
+endmodule
+
+module logic_0 (
+    output a
+);
+  assign a = 0;
+endmodule
+
+module logic_1 (
+    output a
+);
+  assign a = 1;
+endmodule
+
+(* blackbox *)
+module gclkbuff (
+    input  A,
+    output Z
+);
+
+  assign Z = A;
+
+endmodule
