ed8000acd74afd3526288b2cd492def4822cb646
Lilit Hunanyan <lilit.hunanyan@rapidsilicon.com>
2022-04-06 20:41:43 +0500

    Changed the module name from _80_quicklogic_alu to _80_rs_alu.

--- genesis/arith_map.v
+++ genesis/arith_map.v
@@ -14,7 +14,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 (* techmap_celltype = "$alu" *)
-module _80_quicklogic_alu (A, B, CI, BI, X, Y, CO);
+module _80_rs_alu (A, B, CI, BI, X, Y, CO);
 	parameter A_SIGNED = 0;
 	parameter B_SIGNED = 0;
 	parameter A_WIDTH = 2;
