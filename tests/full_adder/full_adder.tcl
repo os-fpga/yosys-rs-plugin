e86d69e350c46085364c12996bef0a620ea1aff2
Lilit Hunanyan <lilit.hunanyan@rapidsilicon.com>
2022-04-08 15:51:42 +0500

    Added the 'end-line' check for the carry flag parse.

--- tests/full_adder/full_adder.tcl
+++ tests/full_adder/full_adder.tcl
@@ -5,38 +5,41 @@ plugin -i synth-rs
 yosys -import  ;# ingest plugin commands
 
 # Equivalence check for adder synthesis for qlf-k6n10
-read_verilog -icells -DWIDTH=4 $::env(DESIGN_TOP).v
+yosys read -define WIDTH=4
+yosys read -sv full_adder.v
 hierarchy -check -top full_adder
 yosys proc
-equiv_opt -assert  -map +/rapidsilicon/genesis/cells_sim.v synth_rs -tech genesis -goal area -carry
+equiv_opt -assert  -map +/rapidsilicon/genesis/cells_sim.v synth_rs -tech genesis -goal area -carry all
 #synth_rs -tech genesis -goal area -top full_adder -carry
 design -load postopt
 yosys cd full_adder
 stat
-select -assert-count 6 t:adder_carry
+select -assert-count 5 t:adder_carry
 
 design -reset
 
 # Equivalence check for subtractor synthesis for qlf-k6n10
-read_verilog -icells -DWIDTH=4 $::env(DESIGN_TOP).v
+yosys read -define WIDTH=4
+yosys read -sv full_adder.v
 hierarchy -check -top subtractor
 yosys proc
-equiv_opt -assert  -map +/rapidsilicon/genesis/cells_sim.v synth_rs -tech genesis -goal area -carry
+equiv_opt -assert  -map +/rapidsilicon/genesis/cells_sim.v synth_rs -tech genesis -goal area -carry no_const
 design -load postopt
 yosys cd subtractor
 stat
-select -assert-count 6 t:adder_carry
+select -assert-count 5 t:adder_carry
 
 design -reset
 
 # Equivalence check for comparator synthesis for qlf-k6n10
-read_verilog -icells -DWIDTH=4 $::env(DESIGN_TOP).v
+yosys read -define WIDTH=4
+yosys read -sv full_adder.v
 hierarchy -check -top comparator
 yosys proc
-equiv_opt -assert  -map +/rapidsilicon/genesis/cells_sim.v synth_rs -tech genesis -goal area -carry
+equiv_opt -assert  -map +/rapidsilicon/genesis/cells_sim.v synth_rs -tech genesis -goal area -carry no
 design -load postopt
 yosys cd comparator
 stat
-select -assert-count 5 t:adder_carry
+select -assert-count 0 t:adder_carry
 
 design -reset
