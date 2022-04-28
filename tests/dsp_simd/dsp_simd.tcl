470e8ff4872d30d7cc8862bc81a8384abb4609df
Lilit Hunanyan <lilit.hunanyan@rapidsilicon.com>
2022-04-28 17:03:49 +0500

    Added tests related to DSP inference.

---
+++ tests/dsp_simd/dsp_simd.tcl
@@ -0,0 +1,72 @@
+# For some tests the equiv_induct pass seems to hang if opt_expr + opt_clean
+# are not invoked after techmapping. Therefore this function is used instead
+# of the equiv_opt pass.
+proc check_equiv {top} {
+    hierarchy -top ${top}
+
+    design -save preopt
+    synth_rs -tech genesis -goal area -de -top ${top}
+    design -stash postopt
+
+    design -copy-from preopt  -as gold A:top
+    design -copy-from postopt -as gate A:top
+
+    techmap -wb -autoproc -map +/rapidsilicon/genesis/cells_sim.v
+    yosys proc
+    opt_expr
+    opt_clean
+
+    async2sync
+    equiv_make gold gate equiv
+    equiv_induct equiv
+    equiv_status -assert equiv
+
+    return
+}
+
+yosys -import
+plugin -i synth-rs
+yosys -import  ;# ingest plugin commands
+
+read_verilog dsp_simd.v
+design -save read
+
+set TOP "simd_mult"
+design -load read
+hierarchy -top $TOP
+check_equiv ${TOP}
+design -load postopt
+select -assert-count 0 t:dsp_t1_20x18x64
+select -assert-count 0 t:dsp_t1_10x9x32
+select -assert-count 1 t:RS_DSP2
+
+set TOP "simd_mult_inferred"
+design -load read
+hierarchy -top $TOP
+check_equiv ${TOP}
+design -load postopt
+yosys cd $TOP
+select -assert-count 0 t:dsp_t1_20x18x64
+select -assert-count 0 t:dsp_t1_10x9x32
+select -assert-count 1 t:RS_DSP2
+
+set TOP "simd_mult_odd"
+design -load read
+hierarchy -top $TOP
+check_equiv ${TOP}
+design -load postopt
+yosys cd $TOP
+select -assert-count 0 t:dsp_t1_20x18x64
+select -assert-count 0 t:dsp_t1_10x9x32
+select -assert-count 2 t:RS_DSP2
+
+set TOP "simd_mult_conflict"
+design -load read
+hierarchy -top $TOP
+check_equiv ${TOP}
+design -load postopt
+yosys cd $TOP
+select -assert-count 0 t:dsp_t1_20x18x64
+select -assert-count 0 t:dsp_t1_10x9x32
+select -assert-count 2 t:RS_DSP2
+
