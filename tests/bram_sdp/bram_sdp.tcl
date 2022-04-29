40a57dda06ae3ce8fa516bd520ef60b409d5fff4
Lilit Hunanyan <lilit.hunanyan@rapidsilicon.com>
2022-04-29 22:04:53 +0500

    Added tests related to BRAM inference.

---
+++ tests/bram_sdp/bram_sdp.tcl
@@ -0,0 +1,50 @@
+yosys -import
+
+plugin -i synth-rs
+yosys -import  ;
+
+read_verilog $::env(DESIGN_TOP).v
+design -save bram_sdp
+
+select BRAM_SDP_32x512
+select *
+synth_rs -tech genesis -goal area -de -top BRAM_SDP_32x512
+opt_expr -undriven
+opt_clean
+stat
+write_verilog sim/bram_sdp_32x512_post_synth.v
+select -assert-count 1 t:TDP36K
+
+select -clear
+design -load bram_sdp
+select BRAM_SDP_16x1024
+select *
+synth_rs -tech genesis -goal area -de -top BRAM_SDP_16x1024
+opt_expr -undriven
+opt_clean
+stat
+write_verilog sim/bram_sdp_16x1024_post_synth.v
+select -assert-count 1 t:TDP36K
+
+select -clear
+design -load bram_sdp
+select BRAM_SDP_8x2048
+select *
+synth_rs -tech genesis -goal area -de -top BRAM_SDP_8x2048
+opt_expr -undriven
+opt_clean
+stat
+write_verilog sim/bram_sdp_8x2048_post_synth.v
+select -assert-count 1 t:TDP36K
+
+select -clear
+design -load bram_sdp
+select BRAM_SDP_4x4096
+select *
+synth_rs -tech genesis -goal area -de -top BRAM_SDP_4x4096
+opt_expr -undriven
+opt_clean
+stat
+write_verilog sim/bram_sdp_4x4096_post_synth.v
+select -assert-count 1 t:TDP36K
+
