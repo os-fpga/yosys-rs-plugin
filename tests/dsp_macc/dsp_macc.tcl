435471e873365bb75f809148d249d0437ab4cf8e
Aram Kostanyan <aram@rapidsilicon.com>
2022-07-16 01:11:04 +0500

    Updated DSP inference - Part2.

--- tests/dsp_macc/dsp_macc.tcl
+++ tests/dsp_macc/dsp_macc.tcl
@@ -1,11 +1,11 @@
 # For some tests the equiv_induct pass seems to hang if opt_expr + opt_clean
 # are not invoked after techmapping. Therefore this function is used instead
 # of the equiv_opt pass.
-proc check_equiv {top} {
+proc check_equiv {top use_cfg_params} {
     hierarchy -top ${top}
 
     design -save preopt
-    synth_rs -tech genesis -goal area -de -top ${top}
+    
     if {${use_cfg_params} == 1} {
         synth_rs -tech genesis -goal area -de -top ${top} -use_dsp_cfg_params
     } else {
@@ -39,35 +39,35 @@ proc check_equiv {top} {
 proc test_dsp_design {top expected_cell_suffix} {
     set TOP ${top}
     # Infer DSP with configuration bits passed through ports
-    # We expect QL_DSP2 cells inferred
+    # We expect RS_DSP2 cells inferred
     set USE_DSP_CFG_PARAMS 0
     design -load read
     hierarchy -top $TOP
     check_equiv ${TOP} ${USE_DSP_CFG_PARAMS}
     design -load postopt
     yosys cd ${top}
-    select -assert-count 1 t:QL_DSP2${expected_cell_suffix}
+    select -assert-count 1 t:RS_DSP2${expected_cell_suffix}
     select -assert-count 1 t:*
 
     # Infer DSP with configuration bits passed through parameters
-    # We expect QL_DSP3 cells inferred
+    # We expect RS_DSP3 cells inferred
     set USE_DSP_CFG_PARAMS 1
     design -load read
     hierarchy -top $TOP
     check_equiv ${TOP} ${USE_DSP_CFG_PARAMS}
     design -load postopt
     yosys cd ${TOP}
-    select -assert-count 1 t:QL_DSP3${expected_cell_suffix}
+    select -assert-count 1 t:RS_DSP3${expected_cell_suffix}
     select -assert-count 1 t:*
 
     return
 }
 
 yosys -import
-plugin -i synth-rs
+if { [info procs synth_rs] == {} } { plugin -i synth-rs}
 yosys -import  ;# ingest plugin commands
 
-yosys read -sv dsp_macc.v
+read_verilog dsp_macc.v
 design -save read
 
 test_dsp_design "macc_simple"               "_MULTACC"
