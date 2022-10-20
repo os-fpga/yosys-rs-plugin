c866c2261115a0746dd09950a7ce33d6a4f12c16
Awais Abbas <awais.abbas@rapidsilicon.com>
2022-10-20 11:33:01 +0500

    condition to check existing file added

--- src/fv/fv.cc
+++ src/fv/fv.cc
@@ -1,12 +1,12 @@
 #include <iostream>
 #include "monitor_synthesis_project_dir.h"
 
-void moniter_netlist(std::string *design_stage,std::string *synthesis_dir){
+bool moniter_netlist(std::string *design_stage,std::string *synthesis_dir){
     std::string nstage_netlist = *synthesis_dir + *design_stage;
     
     monitor_synthesis_project_dir fw{*synthesis_dir,nstage_netlist, std::chrono::milliseconds(5)};
-
-    fw.start_monitoring([] (std::string synthesis_netlist_path, std::string _revised_netlist_, FileStatus status) -> void {
+    bool test;
+    test = fw.start_monitoring([] (std::string synthesis_netlist_path, std::string _revised_netlist_, FileStatus status) -> void {
         if(!std::filesystem::is_regular_file(std::filesystem::path(synthesis_netlist_path)) && status != FileStatus::erased) {
             return;
         }
@@ -16,13 +16,23 @@ void moniter_netlist(std::string *design_stage,std::string *synthesis_dir){
             }
         }
     });
+    return test;
 }
 
 int main() {
 
     std::string synthesis_dir = "/nfs_scratch/scratch/FV/awais/file_watcher/";
-    std::string design_stage = "after_fsm.v";
+    std::string design_stage = "after_fsm1.v";
+    bool net_status;
+    std::filesystem::path p = "/nfs_scratch/scratch/FV/awais/file_watcher/after_fsm.v";
+    net_status = moniter_netlist(&design_stage,&synthesis_dir);
 
-    moniter_netlist(&design_stage,&synthesis_dir);
-
+    if (std::filesystem::exists(p)){
+        remove(p);
+    }
+    else{
+        if (net_status){
+            std::cout<<"File does not exist"<<std::endl;
+        }
+    }
 }
