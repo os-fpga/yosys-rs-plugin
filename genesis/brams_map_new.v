c1133e4901cd3c569b755b89ed6f35faaa7d7684
Lilit Hunanyan <lilit.hunanyan@rapidsilicon.com>
2022-10-25 18:12:50 +0500

    GEMINIEDA-399: Updates the BRAM mapping for SDP cells.

--- genesis/brams_map_new.v
+++ genesis/brams_map_new.v
@@ -246,3 +246,213 @@ module \$__RS_FACTOR_BRAM18_TDP (...);
 		.CLK4()
 	);
 endmodule
+
+module \$__RS_FACTOR_BRAM18_SDP (...);
+	parameter WIDTH = 1; 
+    parameter PORT_B_WR_EN_WIDTH = 4;
+    parameter PORT_A_RD_INIT_VALUE = 1;
+
+    parameter [18431:0] INIT = 18432'bx;
+
+    localparam CLKPOL2 = 1;
+	localparam CLKPOL3 = 1;
+    localparam ABITS = 14;
+
+    input PORT_A_CLK;
+	input PORT_B_CLK;
+
+	input [ABITS-1:0] PORT_A_ADDR;
+	output [WIDTH-1:0] PORT_A_RD_DATA;
+	input PORT_A_RD_EN;
+
+	input [ABITS-1:0] PORT_B_ADDR;
+	input [WIDTH-1:0] PORT_B_WR_DATA;
+	input [PORT_B_WR_EN_WIDTH-1:0] PORT_B_WR_EN;
+
+	BRAM2x18_SDP #(
+		.CFG_ABITS(ABITS),
+		.CFG_DBITS(WIDTH),
+		.CFG_ENABLE_B(PORT_B_WR_EN_WIDTH),
+		.CLKPOL2(CLKPOL2),
+		.CLKPOL3(CLKPOL3),
+		.INIT0(INIT),
+	) _TECHMAP_REPLACE_ (
+		.A1ADDR(PORT_A_ADDR),
+		.A1DATA(PORT_A_RD_DATA),
+		.A1EN(PORT_A_RD_EN),
+		.CLK1(PORT_A_CLK),
+
+		.B1ADDR(PORT_B_ADDR),
+		.B1DATA(PORT_B_WR_DATA),
+		.B1EN(PORT_B_WR_EN),
+		.CLK2(PORT_B_CLK)
+	);
+endmodule
+
+module \$__RS_FACTOR_BRAM36_SDP (...);
+	parameter WIDTH = 1;
+	parameter PORT_B_WR_EN_WIDTH = 4;
+    parameter PORT_A_RD_INIT_VALUE = 1;
+
+	parameter [36863:0] INIT = 36864'bx;
+
+	localparam MODE_36  = 3'b111;	// 36 or 32-bit
+	localparam MODE_18  = 3'b110;	// 18 or 16-bit
+	localparam MODE_9   = 3'b101;	// 9 or 8-bit
+	localparam MODE_4   = 3'b100;	// 4-bit
+	localparam MODE_2   = 3'b010;	// 32-bit
+	localparam MODE_1   = 3'b001;	// 32-bit
+	
+    localparam ABITS = 15;
+
+	input PORT_A_CLK;
+	input PORT_B_CLK;
+
+	input [ABITS-1:0] PORT_A_ADDR;
+	output [WIDTH-1:0] PORT_A_RD_DATA;
+	input PORT_A_RD_EN;
+
+	input [ABITS-1:0] PORT_B_ADDR;
+	input [WIDTH-1:0] PORT_B_WR_DATA;
+	input [PORT_B_WR_EN_WIDTH-1:0] PORT_B_WR_EN;
+
+	wire [14:0] A1ADDR_15;
+	wire [14:0] B1ADDR_15;
+
+	wire [35:0] DOBDO;
+
+	wire [14:ABITS] A1ADDR_CMPL;
+	wire [14:ABITS] B1ADDR_CMPL;
+	wire [35:WIDTH] A1DATA_CMPL;
+	wire [35:WIDTH] B1DATA_CMPL;
+
+	wire [14:0] A1ADDR_TOTAL;
+	wire [14:0] B1ADDR_TOTAL;
+	wire [35:0] A1DATA_TOTAL;
+	wire [35:0] B1DATA_TOTAL;
+
+	wire FLUSH1;
+	wire FLUSH2;
+
+	assign A1ADDR_CMPL = {15-ABITS{1'b0}};
+	assign B1ADDR_CMPL = {15-ABITS{1'b0}};
+
+	assign A1ADDR_TOTAL = {A1ADDR_CMPL, PORT_A_ADDR};
+	assign B1ADDR_TOTAL = {B1ADDR_CMPL, PORT_B_ADDR};
+
+	// Assign read/write data - handle special case for 9bit mode
+	// parity bit for 9bit mode is placed in R/W port on bit #16
+	case (WIDTH)
+		9: begin
+			assign PORT_A_RD_DATA = {A1DATA_TOTAL[16], A1DATA_TOTAL[7:0]};
+			assign B1DATA_TOTAL = {B1DATA_CMPL[35:17], PORT_B_WR_DATA[8], B1DATA_CMPL[16:9], PORT_B_WR_DATA[7:0]};
+		end
+		default: begin
+			assign PORT_A_RD_DATA = A1DATA_TOTAL[WIDTH-1:0];
+			assign B1DATA_TOTAL = {B1DATA_CMPL, PORT_B_WR_DATA};
+		end
+	endcase
+
+	case (WIDTH)
+		1: begin
+			assign A1ADDR_15 = A1ADDR_TOTAL;
+			assign B1ADDR_15 = B1ADDR_TOTAL;
+            defparam _TECHMAP_REPLACE_.MODE_BITS = { 1'b0,
+                11'd10, 11'd10, 4'd0, `MODE_1, `MODE_1, `MODE_1, `MODE_1, 1'd0,
+                12'd10, 12'd10, 4'd0, `MODE_1, `MODE_1, `MODE_1, `MODE_1, 1'd0
+            };
+		end
+
+		2: begin
+			assign A1ADDR_15 = A1ADDR_TOTAL << 1;
+			assign B1ADDR_15 = B1ADDR_TOTAL << 1;
+            defparam _TECHMAP_REPLACE_.MODE_BITS = { 1'b0,
+                11'd10, 11'd10, 4'd0, `MODE_2, `MODE_2, `MODE_2, `MODE_2, 1'd0,
+                12'd10, 12'd10, 4'd0, `MODE_2, `MODE_2, `MODE_2, `MODE_2, 1'd0
+            };
+		end
+
+		4: begin
+			assign A1ADDR_15 = A1ADDR_TOTAL << 2;
+			assign B1ADDR_15 = B1ADDR_TOTAL << 2;
+            defparam _TECHMAP_REPLACE_.MODE_BITS = { 1'b0,
+                11'd10, 11'd10, 4'd0, `MODE_4, `MODE_4, `MODE_4, `MODE_4, 1'd0,
+                12'd10, 12'd10, 4'd0, `MODE_4, `MODE_4, `MODE_4, `MODE_4, 1'd0
+            };
+		end
+		8, 9: begin
+			assign A1ADDR_15 = A1ADDR_TOTAL << 3;
+			assign B1ADDR_15 = B1ADDR_TOTAL << 3;
+            defparam _TECHMAP_REPLACE_.MODE_BITS = { 1'b0,
+                11'd10, 11'd10, 4'd0, `MODE_9, `MODE_9, `MODE_9, `MODE_9, 1'd0,
+                12'd10, 12'd10, 4'd0, `MODE_9, `MODE_9, `MODE_9, `MODE_9, 1'd0
+            };
+		end
+
+		16, 18: begin
+			assign A1ADDR_15 = A1ADDR_TOTAL << 4;
+			assign B1ADDR_15 = B1ADDR_TOTAL << 4;
+            defparam _TECHMAP_REPLACE_.MODE_BITS = { 1'b0,
+                11'd10, 11'd10, 4'd0, `MODE_18, `MODE_18, `MODE_18, `MODE_18, 1'd0,
+                12'd10, 12'd10, 4'd0, `MODE_18, `MODE_18, `MODE_18, `MODE_18, 1'd0
+            };
+		end
+		32, 36: begin
+			assign A1ADDR_15 = A1ADDR_TOTAL << 5;
+			assign B1ADDR_15 = B1ADDR_TOTAL << 5;
+            defparam _TECHMAP_REPLACE_.MODE_BITS = { 1'b0,
+                11'd10, 11'd10, 4'd0, `MODE_36, `MODE_36, `MODE_36, `MODE_36, 1'd0,
+                12'd10, 12'd10, 4'd0, `MODE_36, `MODE_36, `MODE_36, `MODE_36, 1'd0
+            };
+		end
+		default: begin
+			assign A1ADDR_15 = A1ADDR_TOTAL;
+			assign B1ADDR_15 = B1ADDR_TOTAL;
+            defparam _TECHMAP_REPLACE_.MODE_BITS = { 1'b0,
+                11'd10, 11'd10, 4'd0, `MODE_36, `MODE_36, `MODE_36, `MODE_36, 1'd0,
+                12'd10, 12'd10, 4'd0, `MODE_36, `MODE_36, `MODE_36, `MODE_36, 1'd0
+            };
+		end
+	endcase
+
+	assign FLUSH1 = 1'b0;
+	assign FLUSH2 = 1'b0;
+
+	TDP36K #(
+        .INIT_i(INIT),
+         ) _TECHMAP_REPLACE_ (
+		.RESET_ni(1'b1),
+		.WDATA_A1_i(18'h3FFFF),
+		.WDATA_A2_i(18'h3FFFF),
+		.RDATA_A1_o(A1DATA_TOTAL[17:0]),
+		.RDATA_A2_o(A1DATA_TOTAL[35:18]),
+		.ADDR_A1_i(A1ADDR_15),
+		.ADDR_A2_i(A1ADDR_15),
+		.CLK_A1_i(PORT_A_CLK),
+		.CLK_A2_i(PORT_A_CLK),
+		.REN_A1_i(PORT_A_RD_EN),
+		.REN_A2_i(PORT_A_RD_EN),
+		.WEN_A1_i(1'b0),
+		.WEN_A2_i(1'b0),
+		.BE_A1_i({PORT_A_RD_EN, PORT_A_RD_EN}),
+		.BE_A2_i({PORT_A_RD_EN, PORT_A_RD_EN}),
+
+		.WDATA_B1_i(B1DATA_TOTAL[17:0]),
+		.WDATA_B2_i(B1DATA_TOTAL[35:18]),
+		.RDATA_B1_o(DOBDO[17:0]),
+		.RDATA_B2_o(DOBDO[35:18]),
+		.ADDR_B1_i(B1ADDR_15),
+		.ADDR_B2_i(B1ADDR_15),
+		.CLK_B1_i(PORT_B_CLK),
+		.CLK_B2_i(PORT_B_CLK),
+		.REN_B1_i(1'b0),
+		.REN_B2_i(1'b0),
+		.WEN_B1_i(PORT_B_WR_EN[0]),
+		.WEN_B2_i(PORT_B_WR_EN[0]),
+		.BE_B1_i(PORT_B_WR_EN[1:0]),
+		.BE_B2_i(PORT_B_WR_EN[3:2]),
+
+		.FLUSH1_i(FLUSH1),
+		.FLUSH2_i(FLUSH2)
+	);
+endmodule
