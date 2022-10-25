87f5e1eb0411c71a533f8d0930e7c5a8796b16d6
AYYAZ AHMED <aahmed@computefe2.rapidsilicon.local>
2022-10-25 14:34:44 +0500

    removed the simulation model extraction code

--- src/fv/src/synth_formal.cc
+++ src/fv/src/synth_formal.cc
@@ -156,7 +156,7 @@ string tcl_process(std::string design_stage ,std:: string *synth_dir_){
     string rev_net = *synth_dir_+design_stage;
     vector<string> path_revised;
     path_revised.push_back(rev_net);
-    path_revised.push_back("/nfs_scratch/scratch/FV/ayyaz/Gap-Analysis/RTL_Benchmark/Verilog/RS/libcells/dffsre.v");
+    path_revised.push_back(*synth_dir_+"sim_models.v");
     net_status = moniter_netlist(&design_stage,synth_dir_);
 
     std::cout<<"Netlist Status: "<<net_status<<std::endl;
@@ -230,69 +230,6 @@ void process_stage(struct fv_args *stage_arg){
             std::cout<<"DSP inference is turned default"<<std::endl;
             break;
     }
-}
-void writing (std::ifstream& file1,std::ofstream& sim_model,std::string sline, std::string eline)
-{
-bool check;
-std::string line;
-
-//sim_model.open ("sim_models.v",ios::app);
-
-while (getline(file1,line))
-{
-	
-	if (line==sline)
-	{
-		check=true;
-	}
-	if (check)
-	{
-		if (regex_match (line, regex("(.*initial Q = INIT;)") ))
-		sim_model <<"//initial Q = INIT;\n";
-		else
-		sim_model << line+"\n";
-
-        if (line==eline)
-        {
-        check=false;
-        break;
-        }
-	}	
-}
-}
-
-int gen_sim_model()
-{
-std::ifstream file1;
-std::ifstream file2;
-std::ofstream sim_model; 
-file2.open ("/nfs_scratch/scratch/FV/ayyaz/project_fv_synth/yosys_verific_rs/yosys-rs-plugin/genesis/cells_sim.v");
-file1.open ("/nfs_scratch/scratch/FV/ayyaz/project_fv_synth/yosys_verific_rs/yosys/share/simlib.v");
-remove("sim_models.v");
-sim_model.open ("sim_models.v",ios::app);
-if (file1.fail())
-{
-	std::cout<<"File1 failed to open"<<endl;
-	return 1;
-}
-
-if (file2.fail())
-{
-	std::cout<<"File2 failed to open"<<endl;
-	return 1;
-}
-writing (file1,sim_model,"module \\$bmux (A, S, Y);", "endmodule");
-writing (file1,sim_model,"module \\$lut (A, Y);", "endmodule");
-writing (file2,sim_model,"module dffsre(", "endmodule");
-writing (file2,sim_model,"module dffnsre(", "endmodule");
-writing (file2,sim_model,"module latchsre (", "endmodule");
-writing (file2,sim_model,"module latchnsre (", "endmodule");
-writing (file2,sim_model,"module sh_dff(", "endmodule");
-writing (file2,sim_model,"module adder_carry(", "endmodule");
-file1.close();
-file2.close();
-sim_model.close();
-return 1;	
 }
 
 void *run_fv(void* flow) {
