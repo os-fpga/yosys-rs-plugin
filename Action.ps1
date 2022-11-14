c47f0f1a0684cc634648039739e034af30c84544
Nairi-RS <nairi.baveyan@rapidsilicon.com>
2022-11-14 18:22:25 +0400

    Added automation of Windows builds

--- Action.ps1
+++ Action.ps1
@@ -1,7 +1,8 @@
 mkdir pmgen
 wget -nc -O pmgen.py https://raw.githubusercontent.com/YosysHQ/yosys/master/passes/pmgen/pmgen.py
-ls
+
 python3 pmgen.py -o pmgen/rs-dsp-pm.h -p rs_dsp rs_dsp.pmg
-ls
 python3 pmgen.py -o pmgen/rs-dsp-macc.h -p rs_dsp_macc rs-dsp-macc.pmg
-ls
+python3 pmgen.py -o pmgen/rs-bram-asymmetric-wider-write.h -p rs_bram_asymmetric_wider_write rs-bram-asymmetric-wider-write.pmg
+python3 pmgen.py -o pmgen/rs-bram-asymmetric-wider-read.h -p rs_bram_asymmetric_wider_read rs-bram-asymmetric-wider-read.pmg
+
