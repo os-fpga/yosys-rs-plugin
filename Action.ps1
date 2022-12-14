87234c036f3fbee1e944faf0984c4559ca248fca
unknown <nairi.baveyan@rapidsilicon.com>
2022-12-14 19:39:04 +0400

    Added cygwin path

--- Action.ps1
+++ Action.ps1
@@ -1,8 +1,11 @@
 Write-Output "	yosys_rs_plugin start  "
 
-
 mkdir -Force pmgen
+
+$OldPath = $env:PATH
+$env:PATH = "c:\cygwin64\bin"
 wget -nc -O pmgen.py https://raw.githubusercontent.com/YosysHQ/yosys/master/passes/pmgen/pmgen.py
+$env:PATH = $OldPath
 
 Write-Output " Running python3..."
 
@@ -10,5 +13,6 @@ python3 pmgen.py -o pmgen/rs-dsp-pm.h -p rs_dsp rs_dsp.pmg
 python3 pmgen.py -o pmgen/rs-dsp-macc.h -p rs_dsp_macc rs-dsp-macc.pmg
 python3 pmgen.py -o pmgen/rs-bram-asymmetric-wider-write.h -p rs_bram_asymmetric_wider_write rs-bram-asymmetric-wider-write.pmg
 python3 pmgen.py -o pmgen/rs-bram-asymmetric-wider-read.h -p rs_bram_asymmetric_wider_read rs-bram-asymmetric-wider-read.pmg
+
 
 Write-Output "  yosys_rs_plugin end  "
