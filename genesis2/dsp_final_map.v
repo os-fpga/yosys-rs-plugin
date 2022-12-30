1cd69e4ba87e2ea5b8cde6841eed7f3293fe9f7b
Aram Kostanyan <aram@rapidsilicon.com>
2022-12-30 20:43:38 +0500

    Converted DSP MODE_BITS to big endian.

--- genesis2/dsp_final_map.v
+++ genesis2/dsp_final_map.v
@@ -22,20 +22,21 @@ module dsp_t1_20x18x64_cfg_ports (
     input         subtract_i
 );
 
-    parameter [19:0] COEFF_0 = 20'd0;
-    parameter [19:0] COEFF_1 = 20'd0;
-    parameter [19:0] COEFF_2 = 20'd0;
-    parameter [19:0] COEFF_3 = 20'd0;
-    parameter [2:0] OUTPUT_SELECT = 3'b000;
+
+    parameter [0:19] COEFF_0 = 20'd0;
+    parameter [0:19] COEFF_1 = 20'd0;
+    parameter [0:19] COEFF_2 = 20'd0;
+    parameter [0:19] COEFF_3 = 20'd0;
+    parameter [0:2] OUTPUT_SELECT = 3'b000;
     parameter REGISTER_INPUTS = 1'b0;
 
     RS_DSP # (
-        .MODE_BITS          ({REGISTER_INPUTS,
-                              OUTPUT_SELECT,
-                              COEFF_3,
-                              COEFF_2,
+        .MODE_BITS          ({COEFF_0,
                               COEFF_1,
-                              COEFF_0})
+                              COEFF_2,
+                              COEFF_3,
+                              OUTPUT_SELECT,
+                              REGISTER_INPUTS})
     ) _TECHMAP_REPLACE_ (
         .a                  (a_i),
         .b                  (b_i),
