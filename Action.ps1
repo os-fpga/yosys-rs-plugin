0140295c3903f790116d5f9235c78b4780fd9ca3
unknown <nairi.baveyan@rapidsilicon.com>
2023-01-18 11:46:00 +0400

    Running all commands from Cygwin

--- Action.ps1
+++ Action.ps1
@@ -8,15 +8,15 @@ $env:PATH -split ";"
 $Cygwin = $env:PATH + "bash.exe"
 $arg = "-c"
 
-& $Cygwin $arg "get -nc -O pmgen.py https://raw.githubusercontent.com/YosysHQ/yosys/master/passes/pmgen/pmgen.py"
-$env:PATH = $OldPath
+& $Cygwin $arg "wget -nc -O pmgen.py https://raw.githubusercontent.com/YosysHQ/yosys/master/passes/pmgen/pmgen.py"
 
 Write-Output " Running python3..."
 
-python3 pmgen.py -o pmgen/rs-dsp-pm.h -p rs_dsp rs_dsp.pmg
-python3 pmgen.py -o pmgen/rs-dsp-macc.h -p rs_dsp_macc rs-dsp-macc.pmg
-python3 pmgen.py -o pmgen/rs-bram-asymmetric-wider-write.h -p rs_bram_asymmetric_wider_write rs-bram-asymmetric-wider-write.pmg
-python3 pmgen.py -o pmgen/rs-bram-asymmetric-wider-read.h -p rs_bram_asymmetric_wider_read rs-bram-asymmetric-wider-read.pmg
+& $Cygwin $arg "python3 pmgen.py -o pmgen/rs-dsp-pm.h -p rs_dsp rs_dsp.pmg"
+& $Cygwin $arg "python3 pmgen.py -o pmgen/rs-dsp-macc.h -p rs_dsp_macc rs-dsp-macc.pmg"
+& $Cygwin $arg "python3 pmgen.py -o pmgen/rs-bram-asymmetric-wider-write.h -p rs_bram_asymmetric_wider_write rs-bram-asymmetric-wider-write.pmg"
+& $Cygwin $arg "python3 pmgen.py -o pmgen/rs-bram-asymmetric-wider-read.h -p rs_bram_asymmetric_wider_read rs-bram-asymmetric-wider-read.pmg"
 
+$env:PATH = $OldPath
 
 Write-Output "  yosys_rs_plugin end  "
