41193399c7b9130904ec4a67b951eddd848c792a
Lilit Hunanyan <lilit.hunanyan@rapidsilicon.com>
2023-02-08 13:22:32 +0500

    Added the option to the abc call.

--- src/synth_rapidsilicon.cc
+++ src/synth_rapidsilicon.cc
@@ -695,7 +695,7 @@ struct SynthRapidSiliconPass : public ScriptPass {
 
         for (int n=1; n <= maxLoop; n++) { 
 
-            run("abc -dff");   // WARNING: "abc -dff" is very time consuming !!!
+            run("abc -dff -keepff");   // WARNING: "abc -dff" is very time consuming !!!
 
             if (cec)
                 run("write_verilog -noattr -nohex after_abc-dff" + std::to_string(n) + ".v");
