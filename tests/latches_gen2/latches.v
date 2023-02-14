11aae214749b4df24ccd2562a34403c0e33bb10f
Lilit Hunanyan <lilit.hunanyan@rapidsilicon.com>
2023-02-14 19:46:39 +0500

    Categorized the test cases by technology.

---
+++ tests/latches_gen2/latches.v
@@ -0,0 +1,35 @@
+// Copyright (C) 2022 RapidSilicon
+//
+
+module latchp (
+    input d,
+    clk,
+    en,
+    output reg q
+);
+  initial q <= 1'b0;
+  always @* if (en) q <= d;
+endmodule
+
+module latchn (
+    input d,
+    clk,
+    en,
+    output reg q
+);
+  always @* if (!en) q <= d;
+endmodule
+
+module my_latchsre (
+    input d,
+    clk,
+    en,
+    clr,
+    pre,
+    output reg q
+);
+  always @*
+    if (clr) q <= 1'b0;
+    else if (pre) q <= 1'b1;
+    else if (en) q <= d;
+endmodule
