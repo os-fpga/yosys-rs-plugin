2fc7b16c5e80239d50bf90094990f6d5912bc9d2
Ayyaz Ahmed <ayyaz.ahmed@rapidsilicon.com>
2023-07-06 12:06:12 +0500

    Corrected the CLK_BUF_MAP pass placment

--- src/synth_rapidsilicon.cc
+++ src/synth_rapidsilicon.cc
@@ -2068,10 +2068,11 @@ struct SynthRapidSiliconPass : public ScriptPass {
            
            if (!no_iobuf){
                 run("read_verilog -sv -lib "+readIOArgs);
-                run("iopadmap -bits -inpad rs__I_BUF O:I -outpad rs__O_BUF I:O -tinoutpad rs__IOBUF ~T:O:I:IO");
-                run("techmap -map " GET_TECHMAP_FILE_PATH_RS_PRIMITVES(GENESIS_3_DIR,IO_MODEL_FILE));// TECHMAP CELLS
                 run("clkbufmap -buf rs__CLK_BUF O:I");
                 run("techmap -map " GET_TECHMAP_FILE_PATH_RS_PRIMITVES(GENESIS_3_DIR,IO_MODEL_FILE));// TECHMAP CELLS
+                run("iopadmap -bits -inpad rs__I_BUF O:I -outpad rs__O_BUF I:O -tinoutpad rs__IOBUF ~T:O:I:IO");
+                run("techmap -map " GET_TECHMAP_FILE_PATH_RS_PRIMITVES(GENESIS_3_DIR,IO_MODEL_FILE));// TECHMAP CELLS
+
            }
 
 
