0e6895ac7ceae5efd32cf7763d7903ebfb5a73da
Ayyaz Ahmed <ayyaz.ahmed@rapidsilicon.com>
2023-11-23 14:14:16 +0500

    Added support for inference of new RS TDP_RAM primitives

--- genesis3/brams_map_new.v
+++ genesis3/brams_map_new.v
@@ -14,6 +14,10 @@
 module \$__RS_FACTOR_BRAM36_TDP (...);
 parameter INIT = 0;
 parameter WIDTH = 1;
+parameter PORT_D_DATA_WIDTH = 1;
+parameter PORT_B_DATA_WIDTH = 1;
+parameter PORT_A_DATA_WIDTH = 1;
+parameter PORT_C_DATA_WIDTH = 1;
 
 parameter PORT_D_WIDTH = 1;
 parameter PORT_C_WIDTH = 1;
@@ -94,13 +98,40 @@ assign B_ADDR = PORT_C_ADDR == PORT_D_ADDR ? PORT_C_ADDR : PORT_C_RD_EN ? PORT_C
 // PORT A
 case (PORT_A_WIDTH)
 	9: begin
-		assign PORT_A_RD_DATA = {A1DATA_TOTAL[16], A1DATA_TOTAL[7:0]};
+		//assign PORT_A_RD_DATA = {A1DATA_TOTAL[16], A1DATA_TOTAL[7:0]};
+		case (PORT_A_DATA_WIDTH)
+		9: begin
+			assign A1DATA_TOTAL = {A1DATA_TOTAL[35], A1DATA_TOTAL[26], A1DATA_TOTAL[17],PORT_A_RD_DATA[8],A1DATA_TOTAL[34:27],A1DATA_TOTAL[25:18], A1DATA_TOTAL[16:9], PORT_A_RD_DATA[7:0]};
+		end
+		default: begin
+			assign PORT_A_RD_DATA = A1DATA_TOTAL[PORT_A_WIDTH-1:0];
+		end
+	endcase
 	end
 	18: begin
-		assign A1DATA_TOTAL = {A1DATA_TOTAL[35:18],PORT_A_RD_DATA[17], PORT_A_RD_DATA[8],PORT_A_RD_DATA[16:9], PORT_A_RD_DATA[7:0]};
+		//assign A1DATA_TOTAL = {A1DATA_TOTAL[35:18],PORT_A_RD_DATA[17], PORT_A_RD_DATA[8],PORT_A_RD_DATA[16:9], PORT_A_RD_DATA[7:0]};
+		case (PORT_A_DATA_WIDTH)
+			18: begin
+				assign A1DATA_TOTAL = {A1DATA_TOTAL[35],A1DATA_TOTAL[26],PORT_A_RD_DATA[17],PORT_A_RD_DATA[8],A1DATA_TOTAL[34:27],A1DATA_TOTAL[25:18],PORT_A_RD_DATA[16:9], PORT_A_RD_DATA[7:0]};
+			end
+			default: begin
+				assign PORT_A_RD_DATA = A1DATA_TOTAL[PORT_A_WIDTH-1:0];
+			end
+		endcase
 	end
 	36: begin
-		assign A1DATA_TOTAL = {PORT_A_RD_DATA[35],PORT_A_RD_DATA[26], PORT_A_RD_DATA[34:27], PORT_A_RD_DATA[25:18],PORT_A_RD_DATA[17], PORT_A_RD_DATA[8],PORT_A_RD_DATA[16:9], PORT_A_RD_DATA[7:0]};
+		//assign A1DATA_TOTAL = {PORT_A_RD_DATA[35],PORT_A_RD_DATA[26], PORT_A_RD_DATA[34:27], PORT_A_RD_DATA[25:18],PORT_A_RD_DATA[17], PORT_A_RD_DATA[8],PORT_A_RD_DATA[16:9], PORT_A_RD_DATA[7:0]};
+		case(PORT_A_DATA_WIDTH)
+			36:begin
+				assign A1DATA_TOTAL = {PORT_A_RD_DATA[35],PORT_A_RD_DATA[26], PORT_A_RD_DATA[17], PORT_A_RD_DATA[8], PORT_A_RD_DATA[34:27], PORT_A_RD_DATA[25:18],PORT_A_RD_DATA[16:9], PORT_A_RD_DATA[7:0]};
+			end
+			27:begin
+				assign A1DATA_TOTAL = {PORT_A_RD_DATA[35],PORT_A_RD_DATA[26], PORT_A_RD_DATA[17], PORT_A_RD_DATA[8], PORT_A_RD_DATA[34:27], PORT_A_RD_DATA[25:18],PORT_A_RD_DATA[16:9], PORT_A_RD_DATA[7:0]};
+			end
+			default: begin
+				assign PORT_A_RD_DATA = A1DATA_TOTAL[PORT_A_WIDTH-1:0];
+			end
+		endcase
 	end
 	default: begin
 		assign PORT_A_RD_DATA = A1DATA_TOTAL[PORT_A_WIDTH-1:0];
@@ -111,13 +142,40 @@ endcase
 // PORT B
 case (PORT_B_WIDTH)
 	9: begin
-		assign B1DATA_TOTAL = {B1DATA_CMPL[35:17], PORT_B_WR_DATA[8], B1DATA_CMPL[16:9], PORT_B_WR_DATA[7:0]};
+		//assign B1DATA_TOTAL = {B1DATA_CMPL[35:17], PORT_B_WR_DATA[8], B1DATA_CMPL[16:9], PORT_B_WR_DATA[7:0]};
+		case (PORT_B_DATA_WIDTH)
+			9:begin
+				assign B1DATA_TOTAL = {B1DATA_CMPL[35],B1DATA_CMPL[26], B1DATA_CMPL[17], PORT_B_WR_DATA[8],B1DATA_CMPL[34:27],B1DATA_CMPL[25:18] ,PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+			end 
+			default: begin
+				assign B1DATA_TOTAL = {B1DATA_CMPL, PORT_B_WR_DATA};
+			end
+		endcase
 	end
 	18: begin
-		assign B1DATA_TOTAL = {B1DATA_CMPL[35:18], PORT_B_WR_DATA[17], PORT_B_WR_DATA[8], PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+		//assign B1DATA_TOTAL = {B1DATA_CMPL[35:18], PORT_B_WR_DATA[17], PORT_B_WR_DATA[8], PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+		case (PORT_B_DATA_WIDTH)
+			18: begin
+				assign B1DATA_TOTAL = {B1DATA_CMPL[35],B1DATA_CMPL[26], PORT_B_WR_DATA[17], PORT_B_WR_DATA[8],B1DATA_CMPL[34:27],B1DATA_CMPL[25:18] ,PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+			end
+			default: begin
+				assign B1DATA_TOTAL = {B1DATA_CMPL, PORT_B_WR_DATA};
+			end
+		endcase
 	end
 	36: begin
-		assign B1DATA_TOTAL = {PORT_B_WR_DATA[35], PORT_B_WR_DATA[26],PORT_B_WR_DATA[34:27],PORT_B_WR_DATA[25:18],PORT_B_WR_DATA[17], PORT_B_WR_DATA[8], PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+		//assign B1DATA_TOTAL = {PORT_B_WR_DATA[35], PORT_B_WR_DATA[26],PORT_B_WR_DATA[34:27],PORT_B_WR_DATA[25:18],PORT_B_WR_DATA[17], PORT_B_WR_DATA[8], PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+		case(PORT_B_DATA_WIDTH)
+		27:begin
+			assign B1DATA_TOTAL = {PORT_B_WR_DATA[35], PORT_B_WR_DATA[26],PORT_B_WR_DATA[17], PORT_B_WR_DATA[8],PORT_B_WR_DATA[34:27],PORT_B_WR_DATA[25:18], PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+		end
+		36:begin
+			assign B1DATA_TOTAL = {PORT_B_WR_DATA[35], PORT_B_WR_DATA[26],PORT_B_WR_DATA[17], PORT_B_WR_DATA[8],PORT_B_WR_DATA[34:27],PORT_B_WR_DATA[25:18], PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+		end
+		default: begin
+			assign B1DATA_TOTAL = {B1DATA_CMPL, PORT_B_WR_DATA};
+		end
+	endcase
 	end
 	default: begin
 		assign B1DATA_TOTAL = {B1DATA_CMPL, PORT_B_WR_DATA};
@@ -128,13 +186,40 @@ endcase
 // PORT C
 case (PORT_C_WIDTH)
 	9: begin
-		assign PORT_C_RD_DATA = {C1DATA_TOTAL[16], C1DATA_TOTAL[7:0]};
+		//assign PORT_C_RD_DATA = {C1DATA_TOTAL[16], C1DATA_TOTAL[7:0]};
+		case (PORT_C_DATA_WIDTH)
+			9: begin
+				assign C1DATA_TOTAL = {C1DATA_TOTAL[35], C1DATA_TOTAL[26], C1DATA_TOTAL[17], PORT_C_RD_DATA[8], C1DATA_TOTAL[34:27], C1DATA_TOTAL[25:18], C1DATA_TOTAL[16:9], PORT_C_RD_DATA[7:0]};
+			end
+			default: begin
+				assign PORT_C_RD_DATA = C1DATA_TOTAL[PORT_C_WIDTH-1:0];
+			end
+		endcase
 	end
 	18: begin
-		assign C1DATA_TOTAL = {C1DATA_TOTAL[35:18],PORT_C_RD_DATA[17], PORT_C_RD_DATA[8], PORT_C_RD_DATA[16:9], PORT_C_RD_DATA[7:0]};
+		//assign C1DATA_TOTAL = {C1DATA_TOTAL[35:18],PORT_C_RD_DATA[17], PORT_C_RD_DATA[8], PORT_C_RD_DATA[16:9], PORT_C_RD_DATA[7:0]};
+		case (PORT_C_DATA_WIDTH)
+		18	:begin
+			assign C1DATA_TOTAL = {C1DATA_TOTAL[35], C1DATA_TOTAL[26], PORT_C_RD_DATA[17], PORT_C_RD_DATA[8], C1DATA_TOTAL[34:27], C1DATA_TOTAL[25:18], PORT_C_RD_DATA[16:9], PORT_C_RD_DATA[7:0]};
+		end 
+			default: begin
+				assign PORT_C_RD_DATA = C1DATA_TOTAL[PORT_C_WIDTH-1:0];
+			end
+		endcase
 	end
 	36: begin
-		assign C1DATA_TOTAL = {PORT_C_RD_DATA[35], PORT_C_RD_DATA[26], PORT_C_RD_DATA[34:27], PORT_C_RD_DATA[25:18],PORT_C_RD_DATA[17], PORT_C_RD_DATA[8], PORT_C_RD_DATA[16:9], PORT_C_RD_DATA[7:0]};
+		//assign C1DATA_TOTAL = {PORT_C_RD_DATA[35], PORT_C_RD_DATA[26], PORT_C_RD_DATA[34:27], PORT_C_RD_DATA[25:18],PORT_C_RD_DATA[17], PORT_C_RD_DATA[8], PORT_C_RD_DATA[16:9], PORT_C_RD_DATA[7:0]};
+		case(PORT_C_DATA_WIDTH)
+		27:begin
+			assign C1DATA_TOTAL = {PORT_C_RD_DATA[35], PORT_C_RD_DATA[26],PORT_C_RD_DATA[17], PORT_C_RD_DATA[8], PORT_C_RD_DATA[34:27], PORT_C_RD_DATA[25:18], PORT_C_RD_DATA[16:9], PORT_C_RD_DATA[7:0]};
+		end
+		36:begin
+			assign C1DATA_TOTAL = {PORT_C_RD_DATA[35], PORT_C_RD_DATA[26],PORT_C_RD_DATA[17], PORT_C_RD_DATA[8], PORT_C_RD_DATA[34:27], PORT_C_RD_DATA[25:18], PORT_C_RD_DATA[16:9], PORT_C_RD_DATA[7:0]};
+		end
+		default: begin
+			assign PORT_C_RD_DATA = C1DATA_TOTAL[PORT_C_WIDTH-1:0];
+		end
+	endcase
 	end
 	default: begin
 		assign PORT_C_RD_DATA = C1DATA_TOTAL[PORT_C_WIDTH-1:0];
@@ -145,13 +230,40 @@ endcase
 // PORT D
 case (PORT_D_WIDTH)
 	9: begin
-		assign D1DATA_TOTAL = {D1DATA_CMPL[35:17], PORT_D_WR_DATA[8], D1DATA_CMPL[16:9], PORT_D_WR_DATA[7:0]};
+		//assign D1DATA_TOTAL = {D1DATA_CMPL[35:17], PORT_D_WR_DATA[8], D1DATA_CMPL[16:9], PORT_D_WR_DATA[7:0]};
+		case (PORT_D_DATA_WIDTH)
+			9: begin
+				assign D1DATA_TOTAL = {D1DATA_CMPL[35], D1DATA_CMPL[26], D1DATA_CMPL[17], PORT_D_WR_DATA[8], D1DATA_CMPL[34:27], D1DATA_CMPL[25:18], D1DATA_CMPL[16:9], PORT_D_WR_DATA[7:0]};
+			end
+			default: begin
+				assign D1DATA_TOTAL = {D1DATA_CMPL, PORT_D_WR_DATA};
+			end
+		endcase
 	end
 	18: begin
-		assign D1DATA_TOTAL = {D1DATA_CMPL[35:18], PORT_D_WR_DATA[17], PORT_D_WR_DATA[8], PORT_D_WR_DATA[16:9], PORT_D_WR_DATA[7:0]};
+		//assign D1DATA_TOTAL = {D1DATA_CMPL[35:18], PORT_D_WR_DATA[17], PORT_D_WR_DATA[8], PORT_D_WR_DATA[16:9], PORT_D_WR_DATA[7:0]};
+		case (PORT_D_DATA_WIDTH)
+			18:begin
+				assign D1DATA_TOTAL = {D1DATA_CMPL[35], D1DATA_CMPL[26], PORT_D_WR_DATA[17], PORT_D_WR_DATA[8], D1DATA_CMPL[34:27], D1DATA_CMPL[25:18], PORT_D_WR_DATA[16:9], PORT_D_WR_DATA[7:0]};
+			end 
+			default: begin
+				assign D1DATA_TOTAL = {D1DATA_CMPL, PORT_D_WR_DATA};
+			end
+		endcase
 	end
 	36: begin
-		assign D1DATA_TOTAL = {PORT_D_WR_DATA[35], PORT_D_WR_DATA[26], PORT_D_WR_DATA[34:27], PORT_D_WR_DATA[25:18], PORT_D_WR_DATA[17], PORT_D_WR_DATA[8], PORT_D_WR_DATA[16:9], PORT_D_WR_DATA[7:0]};
+		//assign D1DATA_TOTAL = {PORT_D_WR_DATA[35], PORT_D_WR_DATA[26], PORT_D_WR_DATA[34:27], PORT_D_WR_DATA[25:18], PORT_D_WR_DATA[17], PORT_D_WR_DATA[8], PORT_D_WR_DATA[16:9], PORT_D_WR_DATA[7:0]};
+		case(PORT_D_DATA_WIDTH)
+		27:begin
+			assign D1DATA_TOTAL = {PORT_D_WR_DATA[35], PORT_D_WR_DATA[26], PORT_D_WR_DATA[17], PORT_D_WR_DATA[8], PORT_D_WR_DATA[34:27], PORT_D_WR_DATA[25:18], PORT_D_WR_DATA[16:9], PORT_D_WR_DATA[7:0]};
+		end
+		36:begin
+			assign D1DATA_TOTAL = {PORT_D_WR_DATA[35], PORT_D_WR_DATA[26], PORT_D_WR_DATA[17], PORT_D_WR_DATA[8], PORT_D_WR_DATA[34:27], PORT_D_WR_DATA[25:18], PORT_D_WR_DATA[16:9], PORT_D_WR_DATA[7:0]};
+		end
+		default: begin
+			assign D1DATA_TOTAL = {D1DATA_CMPL, PORT_D_WR_DATA};
+		end
+	endcase
 	end
 	default: begin
 		assign D1DATA_TOTAL = {D1DATA_CMPL, PORT_D_WR_DATA};
@@ -164,13 +276,13 @@ endcase
 
 
 // Assigning MODE BITS with respect to Port WIDTHS
-
+/* Commented for new TDP_RAM36k Primitive
 	defparam _TECHMAP_REPLACE_.MODE_BITS = { 1'd0,
 		PORT_A_MODE, PORT_A_MODE, PORT_B_MODE, PORT_B_MODE, 4'd0, 12'b010100000000, 12'b010100000000, 1'd0,
 		PORT_C_MODE, PORT_C_MODE, PORT_D_MODE, PORT_D_MODE, 4'd0, 11'b01010000000, 11'b01010000000, 1'b0
 	};
 
-
+*/
 /*case (WIDTH)
 	1: begin
 		defparam _TECHMAP_REPLACE_.MODE_BITS = { 1'd0,
@@ -225,7 +337,7 @@ endcase*/
 assign SPLIT = 1'b0;
 assign FLUSH1 = 1'b0;
 assign FLUSH2 = 1'b0;
-
+/*
 RS_TDP36K #(
 	.INIT_i(INIT)
 	) _TECHMAP_REPLACE_ (
@@ -261,6 +373,33 @@ RS_TDP36K #(
 
 	.FLUSH1(FLUSH1),
 	.FLUSH2(FLUSH2)
+);*/
+TDP_RAM36K #(
+  .INIT (INIT), // Initial Contents of memory
+  .INIT_PARITY (), // Initial Contents of memory
+  .WRITE_WIDTH_A (PORT_B_WIDTH), // Write data width on port A (1-36)
+  .READ_WIDTH_A (PORT_A_WIDTH), // Read data width on port A (1-36)
+  .WRITE_WIDTH_B (PORT_D_WIDTH), // Write data width on port B (1-36)
+  .READ_WIDTH_B (PORT_C_WIDTH) // Read data width on port B (1-36)
+) _TECHMAP_REPLACE_ (
+  .WEN_A(PORT_B_WR_EN), // Write-enable port A
+  .WEN_B(PORT_D_WR_EN), // Write-enable port B
+  .REN_A(PORT_A_RD_EN), // Read-enable port A
+  .REN_B(PORT_C_RD_EN), // Read-enable port B
+  .CLK_A(PORT_A_CLK), // Clock port A
+  .CLK_B(PORT_C_CLK), // Clock port B
+  .BE_A(B1EN), // Byte-write enable port A
+  .BE_B(D1EN), // Byte-write enable port B
+  .ADDR_A(A_ADDR), // Address port A, align MSBs and connect unused MSBs to logic 0
+  .ADDR_B(B_ADDR), // Address port B, align MSBs and connect unused MSBs to logic 0
+  .WDATA_A({B1DATA_TOTAL[31:0]}), // Write data port A
+  .WPARITY_A({B1DATA_TOTAL[35], B1DATA_TOTAL[34], B1DATA_TOTAL[33], B1DATA_TOTAL[32]}), // Write parity data port A
+  .RDATA_A ({A1DATA_TOTAL[31:0]}), // Read data port A
+  .RPARITY_A ({A1DATA_TOTAL[35], A1DATA_TOTAL[34], A1DATA_TOTAL[33], A1DATA_TOTAL[32]}), // Read parity port A
+  .WDATA_B({D1DATA_TOTAL[31:0]}), // Write data port B
+  .WPARITY_B({D1DATA_TOTAL[35], D1DATA_TOTAL[34], D1DATA_TOTAL[33], D1DATA_TOTAL[32]}), // Write parity port B
+  .RDATA_B ({C1DATA_TOTAL[31:0]}), // Read data port B
+  .RPARITY_B ({C1DATA_TOTAL[35], C1DATA_TOTAL[34], C1DATA_TOTAL[33], C1DATA_TOTAL[32]}) // Read parity port B
 );
 endmodule
 
@@ -273,9 +412,15 @@ module \$__RS_FACTOR_BRAM18_TDP (...);
 	parameter PORT_D_WIDTH = 1;
     parameter PORT_C_WIDTH = 1;
 	
+	parameter PORT_D_DATA_WIDTH = 1;
+	parameter PORT_C_DATA_WIDTH = 1;
+	
     parameter PORT_B_WIDTH = 1;
     parameter PORT_A_WIDTH = 1;
 	
+	parameter PORT_B_DATA_WIDTH = 1;
+	parameter PORT_A_DATA_WIDTH = 1;
+
 	parameter PORT_B_WR_BE_WIDTH = 1;
 	parameter PORT_A_RD_SRST_VALUE = 1;
 	
@@ -409,6 +554,10 @@ module \$__RS_FACTOR_BRAM36_SDP (...);
 	//parameter WIDTH = 1;
 	parameter PORT_B_WIDTH=1;
 	parameter PORT_A_WIDTH=1;
+
+	parameter PORT_A_DATA_WIDTH=1;
+	parameter PORT_B_DATA_WIDTH=1;
+
 	parameter PORT_B_WR_BE_WIDTH = 1;
 
 	parameter [36863:0] INIT = 36864'bx;
@@ -440,9 +589,120 @@ module \$__RS_FACTOR_BRAM36_SDP (...);
 	wire [CFG_ENABLE-1:PORT_B_WR_BE_WIDTH] B1EN_CMPL = {CFG_ENABLE-PORT_B_WR_BE_WIDTH{1'b0}};
 	wire [CFG_ENABLE-1:0] B1EN = {B1EN_CMPL, PORT_B_WR_BE};
 
+	/*Adjusting the bit placement w.r.t new BRAM Primitve TDP_RAM36K*/
+	// PORT A
+case (PORT_A_WIDTH)
+9: begin
+	case (PORT_A_DATA_WIDTH)
+	9: begin
+		assign A1DATA_TOTAL = {A1DATA_TOTAL[35], A1DATA_TOTAL[26], A1DATA_TOTAL[17],PORT_A_RD_DATA[8],A1DATA_TOTAL[34:27],A1DATA_TOTAL[25:18], A1DATA_TOTAL[16:9], PORT_A_RD_DATA[7:0]};
+	end
+	default: begin
+		assign PORT_A_RD_DATA = A1DATA_TOTAL[PORT_A_WIDTH-1:0];
+	end
+endcase
+end
+18: begin
+	case (PORT_A_DATA_WIDTH)
+		18: begin
+			assign A1DATA_TOTAL = {A1DATA_TOTAL[35],A1DATA_TOTAL[26],PORT_A_RD_DATA[17],PORT_A_RD_DATA[8],A1DATA_TOTAL[34:27],A1DATA_TOTAL[25:18],PORT_A_RD_DATA[16:9], PORT_A_RD_DATA[7:0]};
+		end
+		default: begin
+			assign PORT_A_RD_DATA = A1DATA_TOTAL[PORT_A_WIDTH-1:0];
+		end
+	endcase
+end
+36: begin
+	case(PORT_A_DATA_WIDTH)
+		36:begin
+			assign A1DATA_TOTAL = {PORT_A_RD_DATA[35],PORT_A_RD_DATA[26], PORT_A_RD_DATA[17], PORT_A_RD_DATA[8], PORT_A_RD_DATA[34:27], PORT_A_RD_DATA[25:18],PORT_A_RD_DATA[16:9], PORT_A_RD_DATA[7:0]};
+		end
+		27:begin
+			assign A1DATA_TOTAL = {PORT_A_RD_DATA[35],PORT_A_RD_DATA[26], PORT_A_RD_DATA[17], PORT_A_RD_DATA[8], PORT_A_RD_DATA[34:27], PORT_A_RD_DATA[25:18],PORT_A_RD_DATA[16:9], PORT_A_RD_DATA[7:0]};
+		end
+		default: begin
+			assign PORT_A_RD_DATA = A1DATA_TOTAL[PORT_A_WIDTH-1:0];
+		end
+	endcase
+end
+default: begin
+	assign PORT_A_RD_DATA = A1DATA_TOTAL[PORT_A_WIDTH-1:0];
 	
+end
+endcase
+// PORT B
+case (PORT_B_WIDTH)
+	9: begin
+		
+		case (PORT_B_DATA_WIDTH)
+			9:begin
+				assign B1DATA_TOTAL = {B1DATA_CMPL[35],B1DATA_CMPL[26], B1DATA_CMPL[17], PORT_B_WR_DATA[8],B1DATA_CMPL[34:27],B1DATA_CMPL[25:18] ,PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+			end 
+			default: begin
+				assign B1DATA_TOTAL = {B1DATA_CMPL, PORT_B_WR_DATA};
+			end
+		endcase
+	end
+	18: begin
+		
+		case (PORT_B_DATA_WIDTH)
+			18: begin
+				assign B1DATA_TOTAL = {B1DATA_CMPL[35],B1DATA_CMPL[26], PORT_B_WR_DATA[17], PORT_B_WR_DATA[8],B1DATA_CMPL[34:27],B1DATA_CMPL[25:18] ,PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+			end
+			default: begin
+				assign B1DATA_TOTAL = {B1DATA_CMPL, PORT_B_WR_DATA};
+			end
+		endcase
+	end
+	36: begin
+		
+		case(PORT_B_DATA_WIDTH)
+		27:begin
+			assign B1DATA_TOTAL = {PORT_B_WR_DATA[35], PORT_B_WR_DATA[26],PORT_B_WR_DATA[17], PORT_B_WR_DATA[8],PORT_B_WR_DATA[34:27],PORT_B_WR_DATA[25:18], PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+		end
+		36:begin
+			assign B1DATA_TOTAL = {PORT_B_WR_DATA[35], PORT_B_WR_DATA[26],PORT_B_WR_DATA[17], PORT_B_WR_DATA[8],PORT_B_WR_DATA[34:27],PORT_B_WR_DATA[25:18], PORT_B_WR_DATA[16:9], PORT_B_WR_DATA[7:0]};
+		end
+		default: begin
+			assign B1DATA_TOTAL = {B1DATA_CMPL, PORT_B_WR_DATA};
+		end
+	endcase
+	end
+	default: begin
+		assign B1DATA_TOTAL = {B1DATA_CMPL, PORT_B_WR_DATA};
+		
+	end
+endcase
+/*****************************************************************/
+	TDP_RAM36K #(
+		.INIT (INIT), // Initial Contents of memory
+		.INIT_PARITY (), // Initial Contents of memory
+		.WRITE_WIDTH_A (PORT_A_WIDTH), // Write data width on port A (1-36)
+		.READ_WIDTH_A (PORT_A_WIDTH), // Read data width on port A (1-36)
+		.WRITE_WIDTH_B (PORT_B_WIDTH), // Write data width on port B (1-36)
+		.READ_WIDTH_B (PORT_B_WIDTH) // Read data width on port B (1-36)
+	  ) _TECHMAP_REPLACE_ (
+		.WEN_B(PORT_B_WR_EN), // Write-enable port A
+		.REN_B(1'b0), // Read-enable port A
+		.WEN_A(1'b0), // Write-enable port B
+		.REN_A(PORT_A_RD_EN), // Read-enable port B
+		.CLK_B(PORT_B_CLK), // Clock port A
+		.CLK_A(PORT_A_CLK), // Clock port B
+		.BE_B(B1EN), // Byte-write enable port A
+		.BE_A(4'b0), // Byte-write enable port B
+		.ADDR_B(PORT_B_ADDR), // Address port A, align MSBs and connect unused MSBs to logic 0
+		.ADDR_A(PORT_A_ADDR), // Address port B, align MSBs and connect unused MSBs to logic 0
+		.WDATA_B({B1DATA_TOTAL[31:0]}), // Write data port A
+		.WPARITY_B({B1DATA_TOTAL[35], B1DATA_TOTAL[34], B1DATA_TOTAL[33], B1DATA_TOTAL[32]}), // Write parity data port A
+		.RDATA_B ({DOBDO[31:0]}), // Read data port B
+		.RPARITY_B ({DOBDO[35], DOBDO[34], DOBDO[33], DOBDO[32]}), // Read parity port B
+		.WDATA_A({16'hFFFF,16'hFFFF}), // Write data port B
+		.WPARITY_A({4'hF}), // Write parity port B
+		.RDATA_A ({A1DATA_TOTAL[31:0]}), // Read data port A
+		.RPARITY_A ({A1DATA_TOTAL[35], A1DATA_TOTAL[34], A1DATA_TOTAL[33], A1DATA_TOTAL[32]}) // Read parity port A
+	  );
 
-	case (PORT_B_WIDTH)
+	/*case (PORT_B_WIDTH)
 		1: begin
 			case (PORT_A_WIDTH)
 			1: begin
@@ -843,5 +1103,5 @@ endcase
 
 		.FLUSH1(FLUSH1),
 		.FLUSH2(FLUSH2)
-	);
+	);*/
 endmodule
